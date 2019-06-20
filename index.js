789f46518c05eaf1f573ca0d5242fa9e68ef29bc
Nigel Nelson <nigel@prodatakey.com>
2019-06-19 18:03:02 -0600

    noise channel api

--- index.js
+++ index.js
@@ -9,51 +9,25 @@ import { C5, E5, G5 } from './notes.js';
 gameboy.memoryHighWrite(0x25, 0b11111111);
 
 // wave channel
-gameboy.setWaveTable([
+gameboy.wave1.setWaveTable([
 	0x02,0x46,0x8A,0xCE,0xFF,0xFE,0xED,0xDC,0xCB,0xA9,0x87,0x65,0x44,0x33,0x22,0x11
 ]);
 
 const tone = (note) => () => {
 	gameboy.resume();
-	// duty DD, lenght? LLLLLL
-	// gameboy.memoryHighWrite(0x16, 0b10111111)
-	// start volume VVVV, direction A (+/- =1/0), period PPP
-	// gameboy.memoryHighWrite(0x17, 0b11110001)
-	// pitch low
-	// gameboy.memoryHighWrite(0x18, note&255);
-	// trigger 1, something? 0, --- pitch high HHH
-	// gameboy.memoryHighWrite(0x19, 0b10000000 + (note>>8))
+
 	gameboy.pulse2.envelope(15, 1)
 	gameboy.pulse2.duty(2)
 	gameboy.pulse2.play(note);
 
-	// duty DD, lenght? LLLLLL
-	// gameboy.memoryHighWrite(0x11, 0b11111111)
-	// start volume VVVV, direction A (+/- =1/0), period PPP
-	// gameboy.memoryHighWrite(0x12, 0b10010001)
-	// pitch low
-	// gameboy.memoryHighWrite(0x13, (note+10)&255);
-	// trigger 1, something? 0, --- pitch high HHH
-	// gameboy.memoryHighWrite(0x14, 0b10000000 + (note+10>>8))
-	gameboy.pulse1.sweep(7, 7, false)
-	gameboy.pulse1.envelope(12, 1)
+	// gameboy.pulse1.sweep(7, 7, false)
+	gameboy.pulse1.envelope(9, 1)
 	gameboy.pulse1.duty(3)
-	gameboy.pulse1.play(note);
+	gameboy.pulse1.play(note+10);
 
-	// wav
-	// enable channel
-	gameboy.memoryHighWrite(0x1a, 0b10000000)
-	// sound length
-	gameboy.memoryHighWrite(0x1b, 0b11100000)
-	// volume -vv-----
-	gameboy.memoryHighWrite(0x1c, 0b00100000)
+	gameboy.wave1.sampleDownshift(0);
+	gameboy.wave1.play(note, true, 32);
 
-	gameboy.memoryHighWrite(0x1d, note&255);
-	// trigger 1, something? 0, --- pitch high HHH
-	gameboy.memoryHighWrite(0x1e, 0b11000000 + (note>>8))
-
-
-	// noise
 	gameboy.memoryHighWrite(0x20, 0b00111111)
 	gameboy.memoryHighWrite(0x21, 0b01110001)
 	gameboy.memoryHighWrite(0x22, 0b00111111)
