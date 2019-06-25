dddf8f138e429d9b2cd988f86668e177f371b030
Nigel Nelson <nigel@prodatakey.com>
2019-06-25 16:49:54 -0600

    refactor pallet town test

--- testsongs/pallet.js
+++ testsongs/pallet.js
@@ -1,134 +1,6 @@
 import * as gameboy from '../lib/index.js';
 
-function track1() {
-	let octave = 5;
-	let volume = 13;
-	let fade = 3;
-	const data =
-	`notetype 12, 13, 3
-	octave 5
-	D_ 2
-	notetype 12, 10, 3
-	C_ 2
-	notetype 12, 13, 3
-	octave 4
-	B_ 2
-	notetype 12, 11, 3
-	A_ 2
-	notetype 12, 13, 3
-	octave 5
-	G_ 2
-	notetype 12, 11, 3
-	E_ 2
-	notetype 12, 13, 3
-	F# 2
-	E_ 2
-	D_ 6
-	octave 4
-	B_ 2
-	G_ 2
-	G_ 2
-	A_ 2
-	B_ 2
-	octave 5
-	C_ 10
-	octave 4
-	F# 2
-	G_ 2
-	A_ 2
-	B_ 6
-	octave 5
-	C_ 1
-	octave 4
-	B_ 1
-	A_ 8
-	octave 5
-	D_ 2
-	notetype 12, 10, 3
-	C_ 2
-	notetype 12, 13, 3
-	octave 4
-	B_ 2
-	notetype 12, 11, 3
-	octave 5
-	D_ 2
-	notetype 12, 13, 3
-	G_ 2
-	notetype 12, 10, 3
-	F# 2
-	notetype 12, 11, 3
-	F# 2
-	notetype 12, 13, 3
-	G_ 2
-	E_ 6
-	D_ 2
-	D_ 8
-	C_ 2
-	octave 4
-	B_ 2
-	A_ 2
-	G_ 2
-	octave 5
-	D_ 2
-	C_ 2
-	octave 4
-	B_ 2
-	A_ 2
-	G_ 10
-	G_ 2
-	A_ 2
-	B_ 2
-	octave 5
-	C_ 8
-	D_ 6
-	C_ 2
-	octave 4
-	B_ 8
-	rest 2
-	G_ 2
-	A_ 2
-	B_ 2
-	octave 5
-	C_ 4
-	C_ 4
-	D_ 6
-	C_ 1
-	D_ 1
-	octave 4
-	B_ 8
-	rest 2
-	B_ 2
-	A_ 2
-	G_ 2
-	A_ 8
-	E_ 4
-	B_ 4
-	A_ 8
-	G_ 4
-	E_ 4
-	F# 8
-	G_ 4
-	B_ 4
-	B_ 8
-	A_ 8`
-	.split('\n')
-	.map(line => line.trim().split(/,? /g))
-	.map(([cmd, ...args]) => {
-		if (cmd === 'octave') return [() => octave = +args[0]+2];
-		else if (cmd === 'notetype') return [() => ([, volume, fade] = args.map(n => +n))];
-		else if (cmd === 'rest') return [() => gameboy.pulse2({volume:0}), 0x80000*args[0]]
-		else return [() => gameboy.pulse2({ freq: gameboy[cmd.charAt(0)+({_:'','#':'s'})[cmd.charAt(1)]+octave], volume, fade, duty: 2 }), 0x80000*args[0]]
-	})
-	.reduce((arr, x) => arr.concat(x));
-
-	return data;
-}
-
-function track2() {
-	let octave = 5;
-	let volume = 13;
-	let fade = 3;
-	const data =
+const str1 =
 	`octave 3
 	B_ 4
 	octave 4
@@ -268,22 +140,116 @@ function track2() {
 	D_ 2
 	C_ 2
 	D_ 2`
-	.split('\n')
-	.map(line => line.trim().split(/,? /g))
-	.map(([cmd, ...args]) => {
-		if (cmd === 'octave') return [() => octave = +args[0]+2];
-		else if (cmd === 'notetype') return [() => ([, volume, fade] = args.map(n => +n))];
-		else if (cmd === 'rest') return [() => gameboy.pulse1({volume:0}), 0x80000*args[0]]
-		else return [() => gameboy.pulse1({ freq: gameboy[cmd.charAt(0)+({_:'','#':'s'})[cmd.charAt(1)]+octave], volume, fade, duty: 2 }), 0x80000*args[0]]
-	})
-	.reduce((arr, x) => arr.concat(x));
 
-	return data;
-}
+const str2 =
+	`notetype 12, 13, 3
+	octave 5
+	D_ 2
+	notetype 12, 10, 3
+	C_ 2
+	notetype 12, 13, 3
+	octave 4
+	B_ 2
+	notetype 12, 11, 3
+	A_ 2
+	notetype 12, 13, 3
+	octave 5
+	G_ 2
+	notetype 12, 11, 3
+	E_ 2
+	notetype 12, 13, 3
+	F# 2
+	E_ 2
+	D_ 6
+	octave 4
+	B_ 2
+	G_ 2
+	G_ 2
+	A_ 2
+	B_ 2
+	octave 5
+	C_ 10
+	octave 4
+	F# 2
+	G_ 2
+	A_ 2
+	B_ 6
+	octave 5
+	C_ 1
+	octave 4
+	B_ 1
+	A_ 8
+	octave 5
+	D_ 2
+	notetype 12, 10, 3
+	C_ 2
+	notetype 12, 13, 3
+	octave 4
+	B_ 2
+	notetype 12, 11, 3
+	octave 5
+	D_ 2
+	notetype 12, 13, 3
+	G_ 2
+	notetype 12, 10, 3
+	F# 2
+	notetype 12, 11, 3
+	F# 2
+	notetype 12, 13, 3
+	G_ 2
+	E_ 6
+	D_ 2
+	D_ 8
+	C_ 2
+	octave 4
+	B_ 2
+	A_ 2
+	G_ 2
+	octave 5
+	D_ 2
+	C_ 2
+	octave 4
+	B_ 2
+	A_ 2
+	G_ 10
+	G_ 2
+	A_ 2
+	B_ 2
+	octave 5
+	C_ 8
+	D_ 6
+	C_ 2
+	octave 4
+	B_ 8
+	rest 2
+	G_ 2
+	A_ 2
+	B_ 2
+	octave 5
+	C_ 4
+	C_ 4
+	D_ 6
+	C_ 1
+	D_ 1
+	octave 4
+	B_ 8
+	rest 2
+	B_ 2
+	A_ 2
+	G_ 2
+	A_ 8
+	E_ 4
+	B_ 4
+	A_ 8
+	G_ 4
+	E_ 4
+	F# 8
+	G_ 4
+	B_ 4
+	B_ 8
+	A_ 8`;
 
-function track3() {
-	let octave = 5;
-	const data =
+const str3 = 
 	`octave 4
 	G_ 6
 	E_ 6
@@ -327,6 +293,46 @@ function track3() {
 	E_ 8
 	E_ 8
 	F# 8`
+
+function track1() {
+	let octave = 5;
+	let volume = 13;
+	let fade = 3;
+	const data = str1
+		.split('\n')
+	.map(line => line.trim().split(/,? /g))
+	.map(([cmd, ...args]) => {
+		if (cmd === 'octave') return [() => octave = +args[0]+2];
+		else if (cmd === 'notetype') return [() => ([, volume, fade] = args.map(n => +n))];
+		else if (cmd === 'rest') return [() => gameboy.pulse1({volume:0}), 0x80000*args[0]]
+		else return [() => gameboy.pulse1({ freq: gameboy[cmd.charAt(0)+({_:'','#':'s'})[cmd.charAt(1)]+octave], volume, fade, duty: 2 }), 0x80000*args[0]]
+	})
+	.reduce((arr, x) => arr.concat(x));
+
+	return data;
+}
+
+function track2() {
+	let octave = 5;
+	let volume = 13;
+	let fade = 3;
+	const data = str2
+		.split('\n')
+		.map(line => line.trim().split(/,? /g))
+		.map(([cmd, ...args]) => {
+			if (cmd === 'octave') return [() => octave = +args[0]+2];
+			else if (cmd === 'notetype') return [() => ([, volume, fade] = args.map(n => +n))];
+			else if (cmd === 'rest') return [() => gameboy.pulse2({volume:0}), 0x80000*args[0]]
+			else return [() => gameboy.pulse2({ freq: gameboy[cmd.charAt(0)+({_:'','#':'s'})[cmd.charAt(1)]+octave], volume, fade, duty: 2 }), 0x80000*args[0]]
+		})
+		.reduce((arr, x) => arr.concat(x));
+
+	return data;
+}
+
+function track3() {
+	let octave = 5;
+	const data = str3
 	.split('\n')
 	.map(line => line.trim().split(/,? /g))
 	.map(([cmd, ...args]) => {
