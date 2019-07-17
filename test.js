7ff6918cf8c28bc52f9a079d7d015ed39a86c01b
Nigel Nelson <nigel@prodatakey.com>
2019-07-17 13:05:44 -0600

    WIP duty

--- test.js
+++ test.js
@@ -29,22 +29,29 @@ function pulse() {
 	oscillator.connect(gainNode);
 	gainNode.connect(ctx.destination);
 
-	oscillator.type = 'square';
+	const duty = [1/8, 1/4, 1/2]
+		.map(duty => {
+			return ctx.createPeriodicWave(
+				Array(200).fill().map((_,n)=> n === 0 ? 0 : 1/n*Math.sin(Math.PI*n*duty)),
+				Array(200).fill(0)
+			)
+		});
+	oscillator.setPeriodicWave(duty[2]);
 	oscillator.start();
 	gainNode.gain.setValueAtTime(0, ctx.currentTime);
 
 	let t = ctx.currentTime;
 	return {
-		play({ freq=notes.C3, volume=7, fade=1, }) {
+		play({ freq=notes.C3, volume=7, fade=1 }) {
 			// time 
 			if (t < ctx.currentTime) t = ctx.currentTime;
 			// freq
-			oscillator.frequency.setValueAtTime(freq, t);
+			oscillator.frequency.setValueAtTime(0x20000 / (2048-freq), t);
 			// volume + envelope
 			gainNode.gain.cancelAndHoldAtTime(t);
 			let t1 = t;
 			do {
-				gainNode.gain.setValueAtTime(volume/90, t1);
+				gainNode.gain.setValueAtTime(volume/128, t1);
 				t1 += (1/64)*fade;
 				volume -= Math.sign(fade);
 			} while (volume >= 0 && volume <= 15 && fade !== 0)
@@ -57,10 +64,10 @@ function pulse() {
 
 const p1 = pulse();
 
-p1.play({ freq: 0x20000 / (2048-notes.A4) });
+p1.play({ freq: notes.A4 });
 p1.wait(1);
-p1.play({ freq: 0x20000 / (2048-notes.A5) });
+p1.play({ freq: notes.A5 });
 
 setTimeout(() => {
-	gameboy.play(0, [{ freq: notes.A4, volume: 7, fade: 1 }, 0x400000, { freq: notes.A5, volume: 7, fade: 1 }])
+	gameboy.play(0, [{ freq: notes.A4, volume: 7, fade: 1, duty: 2 }, 0x400000, { freq: notes.A5, volume: 7, fade: 1, duty: 2 }])
 }, 200)
