7206dda486e0b73df91933c27e11821246074dc7
Nigel Nelson <nigel@prodatakey.com>
2019-07-29 11:54:25 -0600

    more pkmn sound tests

--- testsongs/pkmn.js
+++ testsongs/pkmn.js
@@ -1,5 +1,5 @@
 import * as notes from '../lib/notes';
-import data from './pkmn/pallet.js';
+import data from './pkmn/routes3.js';
 
 const lines = data.split('\n').map(line => line.trim()).filter(line => line.length > 0);
 let track = [];
@@ -12,10 +12,11 @@ let volume = 13;
 let fade = 3;
 let duty = 2;
 let tempo = 160;
+let notespeed = 12;
 
 for (const line of lines) {
-	if (line.endsWith('::')) {
-		const [, label] = line.match(/(.*?)::$/);
+	if (line.endsWith(':')) {
+		const [, label] = line.match(/(.*?):+$/);
 		labels[label] = track.length;
 		continue;
 	}
@@ -31,17 +32,30 @@ for (const line of lines) {
 	} else if (cmd === 'octave') {
 		octave = args[0]+2;
 	} else if (cmd === 'notetype') {
+		notespeed = args[0];
 		volume = args[1];
 		fade = args[2];
+	} else if (cmd === 'dspeed') {
+		notespeed = args[0];
+	} else if (cmd === 'note') {
+		const note = argStr[0].charAt(0);
+		const sharp = ({'_':'','#':'s'})[argStr[0].charAt(1)];
+		const freq = notes[note + sharp + octave] || notes.C3;
+		track.push({ freq, volume, fade, duty, samples, length: note==='_' ? 0 : Infinity });
+		track.push(800*tempo/0x100000*args[1]);
 	} else if (cmd.match(/^[A-G][_#]$/)) {
 		const note = cmd.charAt(0);
 		const sharp = ({'_':'','#':'s'})[cmd.charAt(1)];
 		const freq = notes[note + sharp + octave];
-		track.push({ freq, volume, fade, duty, samples, length: 320 });
-		track.push(867*tempo/0x100000*args[0]);
+		track.push({ freq, volume, fade, duty, samples });
+		track.push(867*tempo/0x100000*args[0]*notespeed/12);
 	} else if (cmd === 'rest') {
 		track.push({ volume: 0, length: 0 });
-		track.push(867*tempo/0x100000*args[0]);
+		track.push(867*tempo/0x100000*args[0]*notespeed/12);
+	} else if (cmd.startsWith('mutedsnare')) {
+		// TODO find actual mutedsnare sfx
+		track.push({ freq: 1<<8, volume: 6, fade: 1 });
+		track.push(867*tempo/0x100000*args[0]*notespeed/12);
 	} else if (cmd === 'tempo') {
 		tempo = args[0];
 	} else if (cmd === 'vibrato') {
